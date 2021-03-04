a5347da5304bc56949e47b4fb230d7fcb5f41a49
Ariya Hidayat <ariya.hidayat@gmail.com>
2021-03-04 12:20:27 -0800

    Helper function to make an API call

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+const getData = async (searchTerm) => {
+    const response = await fetch(`/search?term=${searchTerm}`);
+    if (!response.ok) {
+        throw new Error(`${response.statusText}`);
+    } else {
+        return await response.json();
+    }
+}
+
 export default function App() {
     return (
         <p>Hello world</p>
