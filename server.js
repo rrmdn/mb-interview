c5012adfd959ee37e58ad1cd4e8b54983d49c1f5
Ariya Hidayat <ariya.hidayat@gmail.com>
2021-03-04 12:19:45 -0800

    Serve hard-coded data

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 
+const data = JSON.parse(require('fs').readFileSync('data.json', 'utf-8'));
+
 app.get('/ping', function (req, res) {
     return res.send(`OK ${Date.now()}`);
+});
+
+app.get('/search', function (req, res) {
+    return res.json(data);
 });
 
 app.listen(process.env.PORT || 8080);
