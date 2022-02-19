bc8e0bf51a48a528dc2c5f2d8aad8a926dfb0515
Rizki Romadhoni <rrmdn@pm.me>
2022-02-19 16:55:39 +0700

    #3 Filter-as-you-type

--- src/App.js
+++ src/App.js
@@ -12,6 +12,13 @@ const getData = async (searchTerm) => {
 };
 
 const useSearch = (term) => {
+  const [debouncedTerm, setDebouncedTerm] = React.useState(term);
+  React.useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 300);
+    return () => clearTimeout(timeout);
+  }, [term]);
   const [results, setResults] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(false);
   const cacheRef = React.useRef(
@@ -19,7 +26,7 @@ const useSearch = (term) => {
     new MapExpire([], { capacity: 3, duration: 5000 })
   );
   React.useEffect(() => {
-    let cached = cacheRef.current.get(term);
+    let cached = cacheRef.current.get(debouncedTerm);
     if (cached) {
       return setResults(cached);
     }
@@ -27,37 +34,35 @@ const useSearch = (term) => {
       setIsLoading(true);
     }, 500);
 
-    getData(term).then((results) => {
-      cacheRef.current.set(term, results);
+    getData(debouncedTerm).then((results) => {
+      cacheRef.current.set(debouncedTerm, results);
       setResults(results);
       clearTimeout(timeout);
       setIsLoading(false);
     });
 
     return () => !!timeout && clearTimeout(timeout);
-  }, [term]);
+  }, [debouncedTerm]);
   return { results, isLoading };
 };
 
 export default function App() {
   const [state, setState] = React.useState({ term: "" });
   const search = useSearch(state.term);
-  const inputRef = React.useRef(document.createElement("input"));
-  const handleSearch = React.useCallback(() => {
-    setState((state) => ({ ...state, term: inputRef.current.value }));
+  const handleSearch = React.useCallback((e) => {
+    const term = e.currentTarget.value;
+    setState((state) => ({ ...state, term }));
   }, []);
+
   return (
     <div style={{ width: "500px" }}>
       <div style={{ display: "flex" }}>
         <div style={{ flex: 1, paddingRight: 20 }}>
           <input
             style={{ width: "100%" }}
-            ref={inputRef}
+            onChange={handleSearch}
             placeholder="search.."
           />
-        </div>
-        <div>
-          <button onClick={handleSearch}>Search</button>
         </div>
       </div>
       <div
